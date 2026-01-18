92b188b723f1adec01abda7b001c6936d4369d05
Axton <axtonurbinaperez@gmail.com>
2026-01-17 21:15:57 -0600

    Implementación de Lista de caracteres para casillas válidas

--- ReversiOthello/src/reversiothello/ReversiOthello.java
+++ ReversiOthello/src/reversiothello/ReversiOthello.java
@@ -4,6 +4,8 @@
  */
 package reversiothello;
 
+import Clases.CharacterList;
+
 /**
  *
  * @author Axton Urbina
@@ -14,7 +16,6 @@ public class ReversiOthello {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
